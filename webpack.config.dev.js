74f89bb06529f54bb5d307616ac2494115937003
liuguili <liuguili@popmart.com>
2022-11-19 22:26:37 +0800

    feat: support alipay

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,45 +1,63 @@
 const webpack = require('webpack');
 const path = require('path');
+module.exports = (env = {}) => {
+  const target = env.target || 'Wechat';
+	const isWechat = target !== 'Alipay';
+	const isAlipay = !isWechat;
 
-module.exports = {
-  mode: 'development',
-  entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'weapp.socket.io.dev.js',
-    libraryTarget: 'umd',
-  },
-  devtool: 'source-map',
-  plugins: [
-    new webpack.NormalModuleReplacementPlugin(/debug/g, process.cwd() + '/support/debug.js'),
-    new webpack.NormalModuleReplacementPlugin(/^ws$/g, process.cwd() + '/src/wx-ws.js'),
-    new webpack.NormalModuleReplacementPlugin(/^.\/websocket-constructor$/g, process.cwd() + '/src/websocket-constructor.js'),
-    new webpack.NormalModuleReplacementPlugin(/^.\/transports\/index$/g, process.cwd() + '/src/transport.js'),
-    new webpack.NormalModuleReplacementPlugin(/^.\/util$/g, process.cwd() + '/src/util.js'),
-    new webpack.NormalModuleReplacementPlugin(/^.\/engine.io-client\/lib\/util$/g, process.cwd() + '/src/util.js'),
-  ],
-  module: {
-    rules: [{
-      test: /engine.io-client\/lib\/socket.js$/,
-      loader: 'string-replace-loader',
-      options: {
-        multiple: [{
-          search: '["polling", "websocket"]',
-          replace: '["websocket"]',
-        }, {
-          search: "['polling', 'websocket']",
-          replace: "['websocket']",
-        }]
-      }
-    }, {
-      test: /engine.io-client\/lib\/transports\/websocket.js$/,
-      loader: 'string-replace-loader',
-      options: {
-        multiple: [{
-          search: "typeof window === 'undefined'",
-          replace: "typeof window === 'undefined' || typeof window !== 'undefined'",
-        }]
-      }
-    }]
+  return {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: `weapp.socket.io.dev.${isWechat ? 'wx' : 'my'}.js`,
+      libraryTarget: 'umd',
+    },
+    devtool: 'source-map',
+    plugins: [
+      new webpack.DefinePlugin({
+        wx: isWechat ? 'wx' : 'my',
+				my: isAlipay ? 'my' : 'wx',
+      }),
+      new webpack.NormalModuleReplacementPlugin(/debug/g, process.cwd() + '/support/debug.js'),
+      new webpack.NormalModuleReplacementPlugin(/^ws$/g, process.cwd() + '/src/wx-ws.js'),
+      new webpack.NormalModuleReplacementPlugin(/^.\/websocket-constructor$/g, process.cwd() + '/src/websocket-constructor.js'),
+      new webpack.NormalModuleReplacementPlugin(/^.\/transports\/index$/g, process.cwd() + '/src/transport.js'),
+      new webpack.NormalModuleReplacementPlugin(/^.\/util$/g, process.cwd() + '/src/util.js'),
+      new webpack.NormalModuleReplacementPlugin(/^.\/engine.io-client\/lib\/util$/g, process.cwd() + '/src/util.js'),
+    ],
+    module: {
+      rules: [{
+        test: /engine.io-client\/lib\/socket.js$/,
+        loader: 'string-replace-loader',
+        options: {
+          multiple: [{
+            search: '["polling", "websocket"]',
+            replace: '["websocket"]',
+          }, {
+            search: "['polling', 'websocket']",
+            replace: "['websocket']",
+          }]
+        }
+      }, {
+        test: /engine.io-client\/lib\/transports\/websocket.js$/,
+        loader: 'string-replace-loader',
+        options: {
+          multiple: [{
+            search: "typeof window === 'undefined'",
+            replace: "typeof window === 'undefined' || typeof window !== 'undefined'",
+          }]
+        }
+      }, {
+        test: /src\/wx-ws.js$/,
+        loader: 'string-replace-loader',
+        options: {
+          multiple: [{
+            search: "PLATFORM_VAR",
+            replace: "my",
+          }]
+        }
+      }]
+    }
   }
-};
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,44 +1,54 @@
 const webpack = require('webpack');
 const path = require('path');
 
-module.exports = {
-  mode: 'production',
-  entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, 'lib'),
-    filename: 'weapp.socket.io.js',
-    libraryTarget: 'umd',
-  },
-  plugins: [
-    new webpack.NormalModuleReplacementPlugin(/debug/, process.cwd() + '/support/noop.js'),
-    new webpack.NormalModuleReplacementPlugin(/^ws$/g, process.cwd() + '/src/wx-ws.js'),
-    new webpack.NormalModuleReplacementPlugin(/^.\/websocket-constructor$/g, process.cwd() + '/src/websocket-constructor.js'),
-    new webpack.NormalModuleReplacementPlugin(/^.\/transports\/index$/g, process.cwd() + '/src/transport.js'),
-    new webpack.NormalModuleReplacementPlugin(/^.\/util$/g, process.cwd() + '/src/util.js'),
-    new webpack.NormalModuleReplacementPlugin(/^.\/engine.io-client\/lib\/util$/g, process.cwd() + '/src/util.js'),
-  ],
-  module: {
-    rules: [{
-      test: /engine.io-client\/lib\/socket.js$/,
-      loader: 'string-replace-loader',
-      options: {
-        multiple: [{
-          search: '["polling", "websocket"]',
-          replace: '["websocket"]',
-        }, {
-          search: "['polling', 'websocket']",
-          replace: "['websocket']",
-        }]
-      }
-    }, {
-      test: /engine.io-client\/lib\/transports\/websocket.js$/,
-      loader: 'string-replace-loader',
-      options: {
-        multiple: [{
-          search: "typeof window === 'undefined'",
-          replace: "typeof window === 'undefined' || typeof window !== 'undefined'",
-        }]
-      }
-    }]
+module.exports = (env = {}) => {
+  const target = env.target || 'Wechat';
+	const isWechat = target !== 'Alipay';
+	const isAlipay = !isWechat;
+
+  return {
+    mode: 'production',
+    entry: './src/index.js',
+    output: {
+      path: path.resolve(__dirname, 'lib'),
+      filename: `weapp.socket.io.${isWechat ? 'wx' : 'my'}.js`,
+      libraryTarget: 'umd',
+    },
+    plugins: [
+      new webpack.DefinePlugin({
+        wx: isWechat ? 'wx' : 'my',
+				my: isAlipay ? 'my' : 'wx',
+      }),
+      new webpack.NormalModuleReplacementPlugin(/debug/, process.cwd() + '/support/noop.js'),
+      new webpack.NormalModuleReplacementPlugin(/^ws$/g, process.cwd() + '/src/wx-ws.js'),
+      new webpack.NormalModuleReplacementPlugin(/^.\/websocket-constructor$/g, process.cwd() + '/src/websocket-constructor.js'),
+      new webpack.NormalModuleReplacementPlugin(/^.\/transports\/index$/g, process.cwd() + '/src/transport.js'),
+      new webpack.NormalModuleReplacementPlugin(/^.\/util$/g, process.cwd() + '/src/util.js'),
+      new webpack.NormalModuleReplacementPlugin(/^.\/engine.io-client\/lib\/util$/g, process.cwd() + '/src/util.js'),
+    ],
+    module: {
+      rules: [{
+        test: /engine.io-client\/lib\/socket.js$/,
+        loader: 'string-replace-loader',
+        options: {
+          multiple: [{
+            search: '["polling", "websocket"]',
+            replace: '["websocket"]',
+          }, {
+            search: "['polling', 'websocket']",
+            replace: "['websocket']",
+          }]
+        }
+      }, {
+        test: /engine.io-client\/lib\/transports\/websocket.js$/,
+        loader: 'string-replace-loader',
+        options: {
+          multiple: [{
+            search: "typeof window === 'undefined'",
+            replace: "typeof window === 'undefined' || typeof window !== 'undefined'",
+          }]
+        }
+      }]
+    }
   }
 };
